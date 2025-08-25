94a2e7d8f87f4fade5e582540b77dbdc02bfe23c
jorgeahn <jorgeahenriqueznovoa@gmail.com>
2025-08-25 17:34:40 -0500

    more drivers are added

---
+++ backend/routes/certificate_type.route.js
@@ -0,0 +1,24 @@
+// backend/routes/certificate_type.route.js
+// Defines URL endpoints for the 'roles' entity and maps them to controller functions.
+import express from 'express';
+import * as certificateTypeController from '../controllers/cetificate_type.controller.js';
+
+const router = express.Router();
+
+// route to get all certificate type and create a new certificate type
+
+router
+    .route('/')
+    .get(certificateTypeController.getAllCertificateType)
+    .post(certificateTypeController.createCertificateType);
+
+
+// Route to get, update, a supecific certificate type by their ID
+
+router
+    .route('/:id')
+    .get(certificateTypeController.getCertificateTypeById)
+    .put(certificateTypeController.updatedCertificateType) // put for full 
+
+
+export default router;
---
+++ backend/routes/vacation_type.route.js
@@ -0,0 +1,23 @@
+// backend/routes/vacation_type.router.js
+// Defines URL endpoints for the 'vacation_type' entity and maps them to controller functions.
+import express from 'express';
+import * as vacationTypeController from '../controllers/vacation_type.controller.js';
+
+const router = express.Router();
+
+// route to get all vacation types and create a new role
+
+router
+    .route('/')
+    .get(vacationTypeController.getAllVactionType)
+    .post(vacationTypeController.createVacationType);
+
+
+// Route to get, update, a supecific vacation type by their ID
+
+router
+    .route('/:id')
+    .get(vacationTypeController.getVacationTypeById)
+    .put(vacationTypeController.updateVacationType) // put for full 
+
+export default router;
